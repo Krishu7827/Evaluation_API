2c24c6fcab6e483c61d1bd11f9c5aa4a56858c1f
Priyanka Jha <krishukumar7827@gmail.com>
2023-03-29 02:27:23 +0530

    final

--- models/Database.js
+++ models/Database.js
@@ -1,6 +1,6 @@
 let mongoose=require("mongoose")
 require("dotenv").config()
-let connection =mongoose.connect(process.env.mongo)
+let connection =mongoose.connect("mongodb+srv://Krishna:Kumar@cluster0.xveo6ik.mongodb.net/PostApp?retryWrites=true&w=majority")
 
 
 module.exports={connection}
